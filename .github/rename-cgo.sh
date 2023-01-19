94354f461d956779e84ed5766eb0e1f348f5d6e8
White12352 <86940955+White12352@users.noreply.github.com>
2023-01-19 17:36:05 +0800

    Update rename-cgo.sh

--- .github/rename-cgo.sh
+++ .github/rename-cgo.sh
@@ -5,16 +5,16 @@ for FILENAME in $FILENAMES
 do
     if [[ $FILENAME =~ "darwin-10.16-arm64" ]];then
         echo "rename darwin-10.16-arm64 $FILENAME"
-        mv $FILENAME clash.meta-darwin-arm64-cgo
+        mv $FILENAME sing-box-darwin-arm64-cgo
     elif [[ $FILENAME =~ "darwin-10.16-amd64" ]];then
         echo "rename darwin-10.16-amd64 $FILENAME"
-        mv $FILENAME clash.meta-darwin-amd64-cgo
+        mv $FILENAME sing-box-darwin-amd64-cgo
     elif [[ $FILENAME =~ "windows-4.0-386" ]];then
         echo "rename windows 386 $FILENAME"
-        mv $FILENAME clash.meta-windows-386-cgo.exe
+        mv $FILENAME sing-box-windows-386-cgo.exe
     elif [[ $FILENAME =~ "windows-4.0-amd64" ]];then
         echo "rename windows amd64 $FILENAME"
-        mv $FILENAME clash.meta-windows-amd64-cgo.exe
+        mv $FILENAME sing-box-windows-amd64-cgo.exe
     elif [[ $FILENAME =~ "linux" ]];then
         echo "rename linux $FILENAME"
         mv $FILENAME $FILENAME-cgo
